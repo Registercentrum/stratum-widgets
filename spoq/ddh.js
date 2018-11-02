de1d700a3f2ebfd4b798520431dea1249e1da152
Magnus Kinnander <magki@rcvg.local>
2018-11-02 14:53:27 +0100

    Refactored

--- spoq/ddh.js
+++ spoq/ddh.js
@@ -1,66 +1,69 @@
 
-var containerHtml= 
-'<div>'+
-'<h1>Antal höfter med LCPD fördelat på kön och ålder (år) vid diagnostillfället</h1>'+
-'<div id="utdataPerthes"></div>'+
-'<div id="utdataPerthes2"></div>'+
-'<h1>Operationsmetod vid primäroperation</h1>'+
-'<div id="utdataPerthesOpmethods"></div>'+
-'<div id="utdataPerthesOpmethods2"></div>'+
-'</div>';
+Ext.application({
+  name: 'app',
+  units: [],
+  launch: function () {
+    var target = (typeof Stratum.containers !== 'undefined') ? Stratum.containers["SPOQ/utdata_DDH"] : 'contentPanel';
 
-/*<h1>Fördelning av icke-operativ/operativ behandling - 30 månader efter diagnos - uppdelat på svårighetsgrad enl. Lateral Pillar-klassifikation</h1>
-<div id="utdataLCPDOpNoOp"></div>
-<div id="utdataLCPDOpNoOp2"></div> */
-
-
-//grå:'#E3E4E5', lila:'#6F6789', oragne:'#EA9430', mörkbblå:'#0075A4', ljusblå:#77C7D2 grön:'#C6D069'
-	Ext.create('Ext.container.Container', {
-    renderTo:
-        (Stratum.containers && Stratum.containers['SPOQ/utdata_perthes']) ||
-        'main-container',
-    html: containerHtml,
-    listeners: {
-        afterRender: function() {
-	var configPerthes = {
-		url: '/stratum/api/statistics/spoq/agesexdistr/?registerid=2190',
-		isPercentage:false,
-		validYnames: ['Pojkar', 'Flickor'],
-		categoryAttrib: 'Ålderskategori',
-		isBarChart: true,
-		legendText: 'Kön',
-		fixedColors:['#0479A6', '#992B4E'],
-		rotateText:false
-		
-	}		
-	var configPerthesOpMethods = {
-		url: '/stratum/api/statistics/spoq/tmpscreening/?registerid=2190&sortorder=3',
-		isPercentage:false,
-		validYnames: ['n'],
-		categoryAttrib: 'opmetod',
-		isBarChart: true,
-		fixedColors:['#0075A4'],
-		legendText:'',
-		rotateText:true
-
-	}
-	var configLCPDOpNoOp = {
-		url: '/stratum/api/statistics/spoq/tmpscreening/?registerid=2190&sortorder=2',
-		isPercentage:false,
-		validYnames: ['Icke-operativ behandling', 'Operativ behandling'],
-		categoryAttrib: 'lpklass',
-		isBarChart: true,
-		fixedColors:['#70688A', '#C6D069', '#0075A4','#EA9430', '#D0D1D2', '#77C7D2'],
-		legendText:' ',
-		rotateText:false
-	}			
-	var renderStatistics=Repository.Local.Methods.renderStatistics;	
-	renderStatistics(true, 'utdataPerthes', configPerthes);
-	renderStatistics(false, 'utdataPerthes2', configPerthes);
-	renderStatistics(true, 'utdataPerthesOpmethods', configPerthesOpMethods);
-	renderStatistics(false, 'utdataPerthesOpmethods2', configPerthesOpMethods);	
-	//RenderSpoqStatistics(true, 'utdataLCPDOpNoOp', configLCPDOpNoOp);
-    //RenderSpoqStatistics(false, 'utdataLCPDOpNoOp2', configLCPDOpNoOp);
-	 },
-    },
+    Ext.create('Ext.container.Container', {
+      items: {
+        html: ''
+          + '<div style="height: 2500px;">'
+          + '<h2>Antal höfter med DDH fördelat på kön och ålder vid diagnostillfället (fördelat per diagnos sent  upptäckt eller komplikation)</h2>'
+          + '<div id="utdataDDH"></div><div id="utdataDDH2"></div>'
+          + '<h2>Undersökningsmetod efter den neonatalt utförda screeningen på BB hos barn med senupptäckt höftledsluxation</h2>'
+          + '<div id="utdataDDHscreening"></div>'
+          + '<div id="utdataDDHscreening2"></div>'
+          + '<h2>Reposition av senupptäckt höftledsluxation?</h2>'
+          + '<div id="utdataDDHscreeningB"></div>'
+          + '<div id="utdataDDHscreeningB2"></div>'
+          + '</div>',
+      },
+      renderTo: target
+    });
+  },
 });
+
+//grå:'#E3E4E5', lila:'#6F6789', orange:'#EA9430', mörkbblå:'#0075A4', ljusblå:#77C7D2 grön:'#C6D069'
+var configDDH = {
+  url: '/stratum/api/statistics/spoq/agesexdistr/?apikey=bK3H9bwaG4o=&rinvoke=1&registerid=2174',
+  isPercentage: false,
+  validYnames: ['Pojkar', 'Flickor'],
+  categoryAttrib: 'Diagnos, ålderskategori',
+  isBarChart: true,
+  legendText: 'Kön',
+  fixedColors: ['#0479A6', '#992B4E'],
+  rotateText: false,
+  withCredentials: true
+}
+var configDDHscreening = {
+  url: '/stratum/api/statistics/spoq/tmpscreening/?apikey=bK3H9bwaG4o=&rinvoke=1&registerid=2174&sortorder=2',
+  isPercentage: true,
+  validYnames: ['%'],
+  categoryAttrib: 'screening',
+  isBarChart: true,
+  legendText: '',
+  //colors: ['#70688A', '#C6D069', '#0075A4','#EA9430', '#D0D1D2', '#77C7D2'],
+  fixedColors: ['#77C7D2'],
+  rotateText: true,
+  withCredentials: true
+}
+var configDDHscreeningB = {
+  url: '/stratum/api/statistics/spoq/tmpscreening/?apikey=bK3H9bwaG4o=&rinvoke=1&registerid=2174&sortorder=3',
+  isPercentage: true,
+  validYnames: ['%'],
+  categoryAttrib: 'DDH_Lux_OpOpenRepos',
+  isBarChart: true,
+  legendText: '',
+  //colors:['#E3E4E5', '#6F6789', '#C6D069'],		
+  fixedColors: ['#77C7D2'],
+  rotateText: false,
+  withCredentials: true
+}
+var renderStatistics = Repository.Local.Methods.renderStatistics;
+renderStatistics(true, 'utdataDDH', configDDH);
+renderStatistics(false, 'utdataDDH2', configDDH);
+renderStatistics(true, 'utdataDDHscreening', configDDHscreening);
+renderStatistics(false, 'utdataDDHscreening2', configDDHscreening);
+renderStatistics(true, 'utdataDDHscreeningB', configDDHscreeningB);
+renderStatistics(false, 'utdataDDHscreeningB2', configDDHscreeningB);
--- spoq/hip.js
+++ spoq/hip.js
@@ -1,72 +1,67 @@
 
-//grå:'#E3E4E5', lila:'#6F6789', oragne:'#EA9430', mörkbblå:'#0075A4', ljusblå:#77C7D2 grön:'#C6D069'
-	var containerHtml=
-	'<div>'+
-	'<h1>Ålder (år) vid operation för höftfyseolys fördelat på kön</h1>'+
-	'<div id="utdataHoft"></div>'+
-	'<div id="utdataHoft2"></div>'+
-	'<h1>Fördelning av glidningsvinkel mätt på sidobild eller frontalplan (grader)</h1>'+
-	'<div id="utdataHoftGlidningsvinkelSidoFrontal"></div>'+
-	'<div id="utdataHoftGlidningsvinkelSidoFrontal2"></div>'+
-	'<h1>Operationsmetod vid operation för höftfyseolys</h1>'+
-	'<div id="utdataHoftscreening"></div>'+
-	'<div id="utdataHoftscreening2"></div>'+
-	'</div>';
-	
-	Ext.create('Ext.container.Container', {
-    renderTo:
-        (Stratum.containers && Stratum.containers['SPOQ/utdata_hoft']) ||
-        'main-container',
-    html: containerHtml,
-    listeners: {
-        afterRender: function() {
+var containerHtml = ''
+  + '<div>'
+  + '<h1>Ålder (år) vid operation för höftfyseolys fördelat på kön</h1>'
+  + '<div id="utdataHoft"></div>'
+  + '<div id="utdataHoft2"></div>'
+  + '<h1>Fördelning av glidningsvinkel mätt på sidobild eller frontalplan (grader)</h1>'
+  + '<div id="utdataHoftGlidningsvinkelSidoFrontal"></div>'
+  + '<div id="utdataHoftGlidningsvinkelSidoFrontal2"></div>'
+  + '<h1>Operationsmetod vid operation för höftfyseolys</h1>'
+  + '<div id="utdataHoftscreening"></div>'
+  + '<div id="utdataHoftscreening2"></div>'
+  + '</div>';
 
-	var configHoft = {
-		url: '/stratum/api/statistics/spoq/agesexdistr/?registerid=2166',
-		isPercentage:false,
-		validYnames: ['Pojkar', 'Flickor'],
-		categoryAttrib: 'Ålderskategori',
-		isBarChart: true,
-		legendText: 'Kön',
-		fixedColors:['#0479A6', '#992B4E'],
-		rotateText:false
-	}
-	
-	var configHoftGlidningsvinkelSidoFrontal = {
-		url: '/stratum/api/statistics/spoq/tmpscreening/?registerid=2166&sortorder=2',
-		isPercentage:false,
-		validYnames: ['10-<=30', '31-<=50', '>50', '<=110', '>110', 'Uppgift saknas'],
-		categoryAttrib: 'metmetod',
-		isBarChart: true,
-		fixedColors:['#6F6789',  '#77C7D2', '#0075A4', '#EA9430', '#992B4E', '#E3E4E5'],
-		legendText:' ',
-		rotateText:false
-	}
-	
-	var configHoftScreening = {
-		url: '/stratum/api/statistics/spoq/tmpscreening/?registerid=2166&sortorder=3',
-		isPercentage:true,
-		validYnames: ['%'],
-		categoryAttrib: 'SCFE_Op_OpMethod',
-		isBarChart: true,
-		legendText: '',
-		//colors: [ '#6F6789',  '#C6D069', '#0075A4', '#EA9430', '#77C7D2',  '#E3E4E5' ],
-		fixedColors: ['#77C7D2'],
-		rotateText:true
-	}
-	 if (!Profile.Context) {
-                document.getElementById('unitHeader1').style.visibility =
-                    'hidden';
-                document.getElementById('unitHeader2').style.visibility =
-                    'hidden';
-            }
-	var renderStatistics=Repository.Local.Methods.renderStatistics;
-	renderStatistics(true, 'utdataHoft', configHoft);
-	renderStatistics(false, 'utdataHoft2', configHoft);
-	renderStatistics(true, 'utdataHoftGlidningsvinkelSidoFrontal', configHoftGlidningsvinkelSidoFrontal);
-	renderStatistics(false, 'utdataHoftGlidningsvinkelSidoFrontal2', configHoftGlidningsvinkelSidoFrontal);		
-	renderStatistics(true, 'utdataHoftscreening', configHoftScreening);
-	renderStatistics(false, 'utdataHoftscreening2', configHoftScreening);
-	},
+Ext.create('Ext.container.Container', {
+  renderTo: (Stratum.containers && Stratum.containers['SPOQ/utdata_hoft']) || 'main-container',
+  html: containerHtml,
+  listeners: {
+    afterRender: function () {
+      var configHoft = {
+        url: '/stratum/api/statistics/spoq/agesexdistr/?registerid=2166',
+        isPercentage: false,
+        validYnames: ['Pojkar', 'Flickor'],
+        categoryAttrib: 'Ålderskategori',
+        isBarChart: true,
+        legendText: 'Kön',
+        fixedColors: ['#0479A6', '#992B4E'],
+        rotateText: false
+      };
+
+      var configHoftGlidningsvinkelSidoFrontal = {
+        url: '/stratum/api/statistics/spoq/tmpscreening/?registerid=2166&sortorder=2',
+        isPercentage: false,
+        validYnames: ['10-<=30', '31-<=50', '>50', '<=110', '>110', 'Uppgift saknas'],
+        categoryAttrib: 'metmetod',
+        isBarChart: true,
+        fixedColors: ['#6F6789', '#77C7D2', '#0075A4', '#EA9430', '#992B4E', '#E3E4E5'],
+        legendText: ' ',
+        rotateText: false
+      };
+
+      var configHoftScreening = {
+        url: '/stratum/api/statistics/spoq/tmpscreening/?registerid=2166&sortorder=3',
+        isPercentage: true,
+        validYnames: ['%'],
+        categoryAttrib: 'SCFE_Op_OpMethod',
+        isBarChart: true,
+        legendText: '',
+        fixedColors: ['#77C7D2'],
+        rotateText: true
+      };
+
+      if (!Profile.Context) {
+        document.getElementById('unitHeader1').style.visibility = 'hidden';
+        document.getElementById('unitHeader2').style.visibility = 'hidden';
+      }
+
+      var renderStatistics = Repository.Local.Methods.renderStatistics;
+      renderStatistics(true, 'utdataHoft', configHoft);
+      renderStatistics(false, 'utdataHoft2', configHoft);
+      renderStatistics(true, 'utdataHoftGlidningsvinkelSidoFrontal', configHoftGlidningsvinkelSidoFrontal);
+      renderStatistics(false, 'utdataHoftGlidningsvinkelSidoFrontal2', configHoftGlidningsvinkelSidoFrontal);
+      renderStatistics(true, 'utdataHoftscreening', configHoftScreening);
+      renderStatistics(false, 'utdataHoftscreening2', configHoftScreening);
     },
+  },
 });
