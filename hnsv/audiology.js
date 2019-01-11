d2866163ad1622b08fb5656be31fa656eb67a77e
Magnus Kinnander <magki@rcvg.local>
2019-01-11 15:47:56 +0100

    Upgraded

--- hnsv/audiology.js
+++ hnsv/audiology.js
@@ -1,111 +1,111 @@
 
 (function() {
-  var container = Stratum.containers && Stratum.containers['HNSV/Audiology'] || 'main-container';
+    var container = Stratum.containers && Stratum.containers['HNSV/Audiology'] || 'main-container';
 
-  var store;
-  Profile.APIKey = 'bK3H9bwaG4o=';
-  store = Ext.create('Ext.data.Store', {
-      
-      fields: ['Ingen','Liten','God','Mycket god', 'UnitName', {
-          name: 'UnitNameReg',
-          convert: function(val, item) {
-              var sum = item.get('Ingen') + item.get('Liten') + item.get('God') + item.get('Mycket god');
-              return Ext.String.format('{0} ({1} reg)', Ext.util.Format.ellipsis(item.get('UnitName'), 25), sum);
-          }
-      }],
-      listeners: {
-          refresh: function(st) {
-              var ingenSum = 0,
-                  litenSum = 0,
-                  godSum = 0,
-                  mGodSum = 0;
-              st.each(function(item) {
-                  ingenSum += item.get('Ingen');
-                  litenSum += item.get('Liten');
-                  godSum += item.get('God');
-                  mGodSum += item.get('Mycket god');
-              });
-              st.add({
-                  Ingen: ingenSum,
-                  Liten: litenSum,
-                  God: godSum,
-                  'Mycket god': mGodSum,
-                  UnitName: 'Riket'
-              });
-          }
-      },
-      sorters: function(a, b) {
-          return a.get('UnitName') === 'Riket' ? 1 : b.get('UnitName') === 'Riket' ? -1 :
-              a.get('UnitName') === b.get('UnitName') ? 0 : a.get('UnitName') > b.get('UnitName') ? -1 : 1;
-      }
-  });
-  ReportManagement.GetReport(3116, null, function(e, r) {
-      if (r && r.result) {
-          if (r.result.success) {
-              store.loadRawData(r.result.data);
-          } else if (r.result.error) {
-              Ext.log({
-                  level: 'error'
-              }, "error on data load: " + r.result.error);
-          }
-      }
-  });
+    var store;
+    Profile.APIKey = 'bK3H9bwaG4o=';
+    store = Ext.create('Ext.data.Store', {
+        
+        fields: ['Ingen','Liten','God','Mycket god', 'UnitName', {
+            name: 'UnitNameReg',
+            convert: function(val, item) {
+                var sum = item.get('Ingen') + item.get('Liten') + item.get('God') + item.get('Mycket god');
+                return Ext.String.format('{0} ({1} reg)', Ext.util.Format.ellipsis(item.get('UnitName'), 25), sum);
+            }
+        }],
+        listeners: {
+            refresh: function(st) {
+                var ingenSum = 0,
+                    litenSum = 0,
+                    godSum = 0,
+                    mGodSum = 0;
+                st.each(function(item) {
+                    ingenSum += item.get('Ingen');
+                    litenSum += item.get('Liten');
+                    godSum += item.get('God');
+                    mGodSum += item.get('Mycket god');
+                });
+                st.add({
+                    Ingen: ingenSum,
+                    Liten: litenSum,
+                    God: godSum,
+                    'Mycket god': mGodSum,
+                    UnitName: 'Riket'
+                });
+            }
+        },
+        sorters: function(a, b) {
+            return a.get('UnitName') === 'Riket' ? 1 : b.get('UnitName') === 'Riket' ? -1 :
+                a.get('UnitName') === b.get('UnitName') ? 0 : a.get('UnitName') > b.get('UnitName') ? -1 : 1;
+        }
+    });
+    ReportManagement.GetReport(3116, null, function(e, r) {
+        if (r && r.result) {
+            if (r.result.success) {
+                store.loadRawData(r.result.data);
+            } else if (r.result.error) {
+                Ext.log({
+                    level: 'error'
+                }, "error on data load: " + r.result.error);
+            }
+        }
+    });
 
-  Ext.create('Ext.container.Container', {
-      width: '100%',
-      layout: 'fit',
-      renderTo: container,
-      items: {
-          xtype: 'chart',
-          height: 700,
-          flipXY: true,
-          animation: false,
-          colors:  ["#4F6228", "#9BBB59", "#F79646", "#C0504D"],
-          shadow: false,
-          store: store,
-          legend: {
-              dock: 'bottom'
-          },
-          axes: [{
-              type: 'category',
-              position: 'left',
-              style: {
-                  strokeStyle: '#ccc'
-              },
-              label: {
-                  textAlign: 'right'
-              }
-          }, {
-              type: 'numeric',
-              position: 'bottom',
-              grid: true,
-              minimum: 0,
-              maximum: 100,
-              style: {
-                  strokeStyle: '#ccc'
-              },
-              renderer: Ext.util.Format.numberRenderer('0%')
-          }],
-          series: [{
-              type: 'bar',
-              fullStack: true,
-              stacked: true,
-              tooltip: {
-                  trackMouse: true,
-                  cls: 'hnsv-tip',
-                  renderer: function(storeItem, item) {
-                      var sum = storeItem.get('Mycket god')+storeItem.get('God')+storeItem.get('Liten')+storeItem.get('Ingen'),
-                          current = storeItem.get(item.field);
-                      this.setHtml(Ext.String.format('{0} (<b>{1}</b>)<hr/><b>{2}</b> av <b>{3}</b>',
-                          storeItem.get('UnitName'),
-                          Ext.util.Format.number(current / sum * 100, '0.0%'),
-                          current,
-                          sum));
-                  }
-              },
-              yField: ['Mycket god', 'God', 'Liten', 'Ingen'],
-              xField: 'UnitNameReg'
-          }]
-      }
-  });
+    Ext.create('Ext.container.Container', {
+        width: '100%',
+        layout: 'fit',
+        renderTo: container,
+        items: {
+            xtype: 'chart',
+            height: 700,
+            flipXY: true,
+            animation: false,
+            colors:  ["#4F6228", "#9BBB59", "#F79646", "#C0504D"],
+            shadow: false,
+            store: store,
+            legend: {
+                dock: 'bottom'
+            },
+            axes: [{
+                type: 'category',
+                position: 'left',
+                style: {
+                    strokeStyle: '#ccc'
+                },
+                label: {
+                    textAlign: 'right'
+                }
+            }, {
+                type: 'numeric',
+                position: 'bottom',
+                grid: true,
+                minimum: 0,
+                maximum: 100,
+                style: {
+                    strokeStyle: '#ccc'
+                },
+                renderer: Ext.util.Format.numberRenderer('0%')
+            }],
+            series: [{
+                type: 'bar',
+                fullStack: true,
+                stacked: true,
+                tooltip: {
+                    trackMouse: true,
+                    cls: 'hnsv-tip',
+                    renderer: function(tooltip, storeItem, item) {
+                        var sum = storeItem.get('Mycket god')+storeItem.get('God')+storeItem.get('Liten')+storeItem.get('Ingen'),
+                            current = storeItem.get(item.field);
+                        tooltip.setHtml(Ext.String.format('{0} (<b>{1}</b>)<hr/><b>{2}</b> av <b>{3}</b>',
+                            storeItem.get('UnitName'),
+                            Ext.util.Format.number(current / sum * 100, '0.0%'),
+                            current,
+                            sum));
+                    }
+                },
+                yField: ['Mycket god', 'God', 'Liten', 'Ingen'],
+                xField: 'UnitNameReg'
+            }]
+        }
+    });
 }());
--- hnsv/pirsbaseline.js
+++ hnsv/pirsbaseline.js
@@ -1,102 +1,102 @@
 
 Ext.onReady(function () {
-  var container = Stratum.containers && Stratum.containers['HNSV/PIRSBaseline'] || 'main-container';
-  // Widget code goes here
-  Profile.APIKey = 'bK3H9bwaG4o=';
-  var store = Ext.create('Ext.data.Store', {
-      fields: [
-          {
-              name: 'PIRS',
-              convert: function (value, record) {
-                  return value === 'NA' ? 15 : value;
-              }
-          },
-          'Reg',
-          'UnitName'
-    ],
-    autoLoad: true,
-    proxy: {
-      type: 'ajax',
-      url: '/stratum/api/statistics/hnsv/pirsny',
-      reader: {
-        type: 'json',
-        rootProperty: 'data'
+    var container = Stratum.containers && Stratum.containers['HNSV/PIRSBaseline'] || 'main-container';
+    // Widget code goes here
+    Profile.APIKey = 'bK3H9bwaG4o=';
+    var store = Ext.create('Ext.data.Store', {
+        fields: [
+            {
+                name: 'PIRS',
+                convert: function (value, record) {
+                    return value === 'NA' ? 15 : value;
+                }
+            },
+            'Reg',
+            'UnitName'
+      ],
+      autoLoad: true,
+      proxy: {
+        type: 'ajax',
+        url: '/stratum/api/statistics/hnsv/pirsny',
+        reader: {
+          type: 'json',
+          rootProperty: 'data'
+        }
+      },
+      sorters: function(a, b) {
+        return a.get('UnitName') === 'Riket' ? 1 : b.get('UnitName') === 'Riket' ? -1 :
+        a.get('UnitName') === b.get('UnitName') ? 0 : a.get('UnitName') > b.get('UnitName') ? -1 : 1;
       }
-    },
-    sorters: function(a, b) {
-      return a.get('UnitName') === 'Riket' ? 1 : b.get('UnitName') === 'Riket' ? -1 :
-      a.get('UnitName') === b.get('UnitName') ? 0 : a.get('UnitName') > b.get('UnitName') ? -1 : 1;
-    }
 
-  })
+    })
 //     window.store = store;
 
-  Ext.create('Ext.container.Container', {
-      width: '100%',
-      layout: 'fit',
-      renderTo: container,
-      items: {
-          insetPadding: 20,
-          xtype: 'chart',
-          height: 700,
-          flipXY: true,
-          animation: false,
-          colors: ['#3CB6CE', '#4BACC6', '#1e833f'],
-          shadow: false,
-          store: store,
-          axes: [{
-              type: 'category',
-              position: 'left',
-              fields: ['UnitName'],
-              style: {
-                  strokeStyle: '#ccc'
-              },
-              label: {
-                  textAlign: 'right'
-              }
-          }, {
-              type: 'numeric',
-              position: 'bottom',
-              grid: true,
-              minimum: 0,
-              maximum: 100,
-              majorTickSteps: 10,
-              minorTickSteps: 5,
-              style: {
-                  strokeStyle: '#ccc'
-              }
-          }],
-          series: [{
-              type: 'bar',
-              renderer: function(sprite, config, rendererData, index) {
-                  var field = 'Reg',
-                      store = rendererData.store;
-                  if (store.getAt(index).get(field) < 5) {
-                      return {
-                          width: 0
-                      };
-                  }
-              },
-              tooltip: {
-                  trackMouse: true,
-                  cls: 'hnsv-tip',
-                  renderer: function(storeItem, item) {
-                      this.setHtml(storeItem.get('UnitName') + '<hr/><b>' + Ext.util.Format.number(storeItem.get('PIRS'), '0') + '</b> i snittbetyg bland <b>' + storeItem.get('Reg') + '</b> svarande patienter.');
-                  }
-              },
-              label: {
-                  display: 'insideStart',
-                  field: 'Reg',
-                  renderer: function(value, sprite, config, rendererData, index) {
-                      var hideUnder = 5;
-                      return (value <= hideUnder ? '<' + hideUnder : Ext.util.Format.number(value, '0')) + ' reg.';
-                  },
-                  contrast: true
-              },
-              yField: 'PIRS',
-              xField: 'UnitName'
-          }]
-      }
-  });
+    Ext.create('Ext.container.Container', {
+        width: '100%',
+        layout: 'fit',
+        renderTo: container,
+        items: {
+            insetPadding: 20,
+            xtype: 'chart',
+            height: 700,
+            flipXY: true,
+            animation: false,
+            colors: ['#3CB6CE', '#4BACC6', '#1e833f'],
+            shadow: false,
+            store: store,
+            axes: [{
+                type: 'category',
+                position: 'left',
+                fields: ['UnitName'],
+                style: {
+                    strokeStyle: '#ccc'
+                },
+                label: {
+                    textAlign: 'right'
+                }
+            }, {
+                type: 'numeric',
+                position: 'bottom',
+                grid: true,
+                minimum: 0,
+                maximum: 100,
+                majorTickSteps: 10,
+                minorTickSteps: 5,
+                style: {
+                    strokeStyle: '#ccc'
+                }
+            }],
+            series: [{
+                type: 'bar',
+                renderer: function(sprite, config, rendererData, index) {
+                    var field = 'Reg',
+                        store = rendererData.store;
+                    if (store.getAt(index).get(field) < 5) {
+                        return {
+                            width: 0
+                        };
+                    }
+                },
+                tooltip: {
+                    trackMouse: true,
+                    cls: 'hnsv-tip',
+                    renderer: function(tooltip, storeItem, item) {
+                        tooltip.setHtml(storeItem.get('UnitName') + '<hr/><b>' + Ext.util.Format.number(storeItem.get('PIRS'), '0') + '</b> i snittbetyg bland <b>' + storeItem.get('Reg') + '</b> svarande patienter.');
+                    }
+                },
+                label: {
+                    display: 'insideStart',
+                    field: 'Reg',
+                    renderer: function(value, sprite, config, rendererData, index) {
+                        var hideUnder = 5;
+                        return (value <= hideUnder ? '<' + hideUnder : Ext.util.Format.number(value, '0')) + ' reg.';
+                    },
+                    contrast: true
+                },
+                yField: 'PIRS',
+                xField: 'UnitName'
+            }]
+        }
+    });
 
 });
