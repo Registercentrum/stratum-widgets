609cf5d9ba6459b4f986505c7458ad7158c231b7
Fredrik Heidgert <fredrik.heidgert@registercentrum.se>
2020-01-13 14:21:52 +0100

    Rewrote screening and subject logs for Hipsther in a modularized fashion

---
+++ sfr/hipsther/screening-log/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'hipsther.screening-log.bundle.js'
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            }
+        ]
+    }
+};
