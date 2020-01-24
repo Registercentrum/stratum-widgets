85b4a3e85cdbba34a6f9ba7d6037d5edcff55fc2
Fredrik Heidgert <fredrik.heidgert@vgregion.se>
2020-01-24 08:31:53 +0100

    Work on RRCT progress widget

--- sfr/rrct/progress-widget/src/index.js
+++ sfr/rrct/progress-widget/src/index.js
@@ -1,16 +1,50 @@
+import * as api from "./api";
+import { renderUI } from "./ui";
 
-// Container: DOM container to render to
-// BaseUrl: Base URL to get data from
-
-if(typeof RrctProgressWidgetConfig !== "object") {
+if(typeof window.RrctProgressWidgetConfig !== "object") {
     throw "No configuration was found for RRCT Progress Widget!";
 }
 
-if(!RrctProgressWidgetConfig.Container) {
+var config = window.RrctProgressWidgetConfig;
+
+if(!config.container) {
     throw "No DOM container was specified!";
 }
 
-if(!RrctProgressWidgetConfig.BaseUrl) {
+if(!config.baseUrl) {
     throw "RRCT base url was not set!";
 }
 
+api.setRrctBaseUrl(config.baseUrl);
+
+function simpleCompare(value1, value2) {
+    if(value1 == value2) return 0;
+    if(value1 > value2) return -1;
+    return 1;
+}
+
+api.getCandidates()
+    .then(function(candidates) {
+        window._candidates = candidates;
+        var includedCandidates = Ext.Array.filter(candidates, function(item) {
+            return item.Included;
+        });
+        var latestIncludedCandidate = Ext.Array.sort(includedCandidates, function(item1, item2) {
+            return simpleCompare(
+                Ext.Date.parse(item1.Screening.DateConsidered, "Y-m-dTH:i:s"),
+                Ext.Date.parse(item2.Screening.DateConsidered, "Y-m-dTH:i:s"));
+        })[0];
+
+        api.getUnit(latestIncludedCandidate.Screening.UnitId).then(function(unit) {
+            var model = {
+                numIncluded: includedCandidates.length,
+                latestIncludedUnitName: unit.UnitName,
+                latestInclusionDate: latestIncludedCandidate.Screening.DateConsidered
+            }
+    
+            renderUI(model, config.container);
+        });
+    }, function(error) {
+        console.error("An error occurred.");
+        console.error(error);
+    });
---
+++ sfr/rrct/progress-widget/src/ui.js
@@ -0,0 +1,62 @@
+
+export function renderUI(model, container) {
+    var formattedDate = Ext.Date.format(new Date(model.latestInclusionDate), "Y-m-d H:i");
+    Ext.create("Ext.container.Container", {
+        renderTo: container,
+        margin: "0 0 20 0",
+        items: [
+            getChart(model.numIncluded), {
+                xtype: "container",
+                html: `
+                    <p>Senast inkluderande enhet:</p>
+                    <p>
+                        <strong style="font-size: 16px;">${model.latestIncludedUnitName}</strong><br>
+                        ${formattedDate}
+                    </p>
+                `
+            }
+        ]
+    });
+}
+
+function getChart(numIncluded) {
+    return Ext.create('Ext.chart.CartesianChart', {
+        width: 150,
+        height: 300,
+        border: false,
+        background: "#f1f1f1",
+        animation: {
+            easing: 'backOut',
+            duration: 5000
+        },
+        store: {
+            fields: ['name', 'numIncluded'],
+            data: [
+                {"name": "", "numIncluded": numIncluded}
+            ]
+        },  
+        axes: [{
+            type: 'numeric',
+            position: 'left',
+            maximum: 1440,
+            majorTickSteps: 10,
+            minorTickSteps: 1
+        }, {
+            type: 'category',
+            position: 'bottom'
+        }],
+        series: [{
+            type: 'bar',
+            xField: 'name',
+            yField: ['numIncluded'],
+            axis: 'bottom',
+            colors: ["#005B91"],
+            tooltip: {
+                trackMouse: true,
+                renderer: function (toolTip, record) {
+                    toolTip.setHtml(record.get('numIncluded') + ' patienter');
+                }
+            }
+       }]
+    });
+}
