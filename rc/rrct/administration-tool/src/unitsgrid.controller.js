f586aa63e6f3c98a196156aff1f1b7da668b915a
Fredrik Heidgert <fredrik.heidgert@vgregion.se>
2020-04-09 10:14:36 +0200

    Enable/disable action buttons based on selection

--- rc/rrct/administration-tool/src/unitsgrid.controller.js
+++ rc/rrct/administration-tool/src/unitsgrid.controller.js
@@ -7,4 +7,15 @@ Ext.define('RC.RRCTAdministration.controller.Units', {
     deactivate: function() {
         console.log('deactivate');
     },
+    onSelectionChange: function (component, record, index, eOpts) {
+        //console.log();
+        if(record[0].data.Enabled) {
+            this.lookup('activateUnitButton').disable();
+            this.lookup('deactivateUnitButton').enable();
+        }
+        else {
+            this.lookup('activateUnitButton').enable();
+            this.lookup('deactivateUnitButton').disable();            
+        }
+    },
 });
