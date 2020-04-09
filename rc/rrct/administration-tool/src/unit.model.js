a51acecb5331fac3afeeee0511e8641a676172d1
Fredrik Heidgert <fredrik.heidgert@vgregion.se>
2020-04-09 11:12:01 +0200

    Toggle button state based on action

--- rc/rrct/administration-tool/src/unit.model.js
+++ rc/rrct/administration-tool/src/unit.model.js
@@ -1,6 +1,7 @@
 
 Ext.define("RC.RRCTAdministration.model.Unit", {
     extend: "Ext.data.Model",
+    idProperty: 'UnitId',
     fields: [ 
         { name: "UnitId" }, 
         { name: "UnitName" }, 
--- rc/rrct/administration-tool/src/unitsgrid.controller.js
+++ rc/rrct/administration-tool/src/unitsgrid.controller.js
@@ -1,21 +1,60 @@
+import config from "./config";
+
 Ext.define('RC.RRCTAdministration.controller.Units', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.units',
     activate: function() {
-        console.log('activate');
+        var unit = this.view.selection.data;
+        var record = this.getView().getStore().getById(unit.UnitId);
+        var controller = this;
+        Ext.MessageBox.show({
+            title: 'Aktivera enhet',
+            msg: `Är du säker på att du vill aktivera enheten ${unit.UnitName} i studien ${config.studyName}?`,
+            buttons: Ext.MessageBox.YESNO,
+            buttonText: {
+                yes: 'Ja',
+                no: 'Nej'
+            },
+            fn: function(response) {
+                if (response == 'yes') {
+                    record.set('Enabled', true);
+                    setButtonState(false, controller);
+                }
+            }
+        });
     },
     deactivate: function() {
-        console.log('deactivate');
+        var unit = this.view.selection.data;
+        var record = this.getView().getStore().getById(unit.UnitId);
+        var controller = this;
+        Ext.MessageBox.show({
+            title: 'Inaktivera enhet',
+            msg: `Är du säker på att du vill inaktivera enheten ${unit.UnitName} i studien ${config.studyName}?`,
+            buttons: Ext.MessageBox.YESNO,
+            buttonText: {
+                yes: 'Ja',
+                no: 'Nej'
+            },
+            fn: function(response) {
+                if (response == 'yes') {
+                    record.set('Enabled', false);
+                    setButtonState(true, controller);
+                }
+            }
+        });
     },
     onSelectionChange: function (component, record, index, eOpts) {
-        //console.log();
-        if(record[0].data.Enabled) {
-            this.lookup('activateUnitButton').disable();
-            this.lookup('deactivateUnitButton').enable();
-        }
-        else {
-            this.lookup('activateUnitButton').enable();
-            this.lookup('deactivateUnitButton').disable();            
-        }
+        setButtonState(!record[0].data.Enabled, this);
     },
 });
+
+function setButtonState(activateEnabled, controller) {
+    if(activateEnabled) {
+        controller.lookup('activateUnitButton').enable();
+        controller.lookup('deactivateUnitButton').disable();            
+    }
+    else {
+        controller.lookup('activateUnitButton').disable();
+        controller.lookup('deactivateUnitButton').enable();
+    }
+}
