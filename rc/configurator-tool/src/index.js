b6a08245f517e2d08db85c7505dd2f4a628996d4
Fredrik Heidgert <fredrik.heidgert@vgregion.se>
2020-04-20 09:19:25 +0200

    Removed unnecessary IIFE

--- rc/configurator-tool/src/index.js
+++ rc/configurator-tool/src/index.js
@@ -7,41 +7,26 @@ import "./views/default.view"
 import "./views/forms.view"
 import "./views/form-detail.view"
 
-(function(_profile) {
-    "use strict";
+var mainItem = { 
+    title: 'Registerkonfigurering', 
+    xtype: 'mainview',
+};
 
-    function init() {
+if(!Profile.Context) {
+    mainItem = {
+        xtype: "component",
+        html: '<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation-triangle pull-right"></i>Du måste vara inloggad för att arbeta med konfiguratörsverktyget.</div>'
+    };
+}
 
-        var mainItem = { 
-            title: 'Registerkonfigurering', 
-            xtype: 'mainview',
-        };
-
-        if(!_profile.Context) {
-            mainItem = {
-                xtype: "component",
-                html: '<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation-triangle pull-right"></i>Du måste vara inloggad för att arbeta med konfiguratörsverktyget.</div>'
-            };
-        }
-
-        Ext.application({
-            name: 'ConfiguratorToolApp',
-            defaultToken : 'default',
-            launch: function() {
-                Ext.create('Ext.panel.Panel', {
-                    renderTo: 'sw-konfiguratoersverktyg',
-                    items: [ mainItem ]
-                });
-            }            
+Ext.application({
+    name: 'ConfiguratorToolApp',
+    defaultToken : 'default',
+    launch: function() {
+        Ext.create('Ext.panel.Panel', {
+            frame: false,
+            renderTo: 'sw-konfiguratoersverktyg',
+            items: [ mainItem ]
         });
-
-    }
-
-    Ext.onReady(function() {
-        init();
-    });
-    
-})(Profile);
-// SiteId: 100
-// WidgetId: RC/ConfiguratorTool
-// WidgetName: Konfiguratörsverktyg
+    }            
+});
